17bf73e831a40fd2dd6ed3dbcb78824fbc3fe511
keiyakato <keimusic1015@softbank.ne.jp>
2018-08-15 17:48:12 +0900

    初期状態

---
+++ dance_practice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dance_practice
+//
+//  Created by 加藤景也 on 2018/08/15.
+//  Copyright © 2018年 加藤景也. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
